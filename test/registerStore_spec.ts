8488d4f7d273736efa3042da8ed6aeff434a524f
Stephan Smola <smola@farbspender.de>
2021-12-13 15:14:00 +0100

    Simple register store

--- test/registerStore_spec.ts
+++ test/registerStore_spec.ts
@@ -1,10 +1,20 @@
 import {describe, it} from "mocha";
 import {expect} from "chai"
 
-describe('Array', function () {
-    describe('#indexOf()', function () {
-        it('should return -1 when the value is not present', function () {
-            expect([1, 2, 3].indexOf(4)).to.equal(-1);
-        });
+import {RegisterStore} from "../src/RegisterStore";
+
+describe('Register store',  ()  => {
+
+    it('can store arbitrary values to a name', () => {
+
+        const rs = new RegisterStore();
+
+        rs.setRegister("test1", "TestValue");
+        rs.setRegister("test2", 1234);
+        rs.setRegister("test3", { a: 12});
+
+        expect(rs.getRegister("test1")).to.equal("TestValue");
+        expect(rs.getRegister("test2")).to.equal(1234);
+        expect(rs.getRegister("test3")).to.deep.equal({a: 12 })
     });
 });
