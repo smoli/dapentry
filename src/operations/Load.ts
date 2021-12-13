c3043a0dd3be8a99c2d03c86f6679c9e42dbefc9
Stephan Smola <smola@farbspender.de>
2021-12-13 23:40:41 +0100

    Load Operation

---
+++ src/operations/Load.ts
@@ -0,0 +1,20 @@
+import {Operation} from "../Operation";
+import {dbg} from "../dbg";
+import {Parameter} from "../Parameter";
+
+export class Load extends Operation {
+
+    private readonly _target: Parameter;
+    private readonly _value: Parameter;
+
+    constructor(opcode, target: Parameter, value: Parameter) {
+        super(opcode);
+        this._target = target;
+        this._value = value;
+    }
+
+    async execute(interpreter): Promise<any> {
+        interpreter.setRegister(this._target.name, this._value.value);
+    }
+
+}
