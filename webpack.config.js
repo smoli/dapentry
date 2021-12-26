1a66c1fae8c016e79eae4d4673d315b26a9ce143
Stephan Smola <smola@farbspender.de>
2021-12-26 08:03:22 +0100

    Switch transpilation to babel
    This allows for using babel-plugins and presets.

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
         rules: [
             {
                 test: /\.tsx?$/,
-                use: 'ts-loader',
+                use: 'babel-loader',
                 exclude: /node_modules/,
             }
         ]
