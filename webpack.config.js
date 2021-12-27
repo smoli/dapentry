d4a61a9da50ef4bda78a934cb1cc833451f154b4
Stephan Smola <smola@farbspender.de>
2021-12-27 06:12:32 +0100

    UI5 get babel transpilation running.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const OpenUI5Plugin = require('openui5-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -13,11 +14,37 @@ module.exports = {
                 test: /\.tsx?$/,
                 use: 'babel-loader',
                 exclude: /node_modules/,
-            }
+            },
+            {
+                test: /manifest\.json$/,
+                type: "javascript/auto",
+                use: {
+                    loader: "openui5-manifest-loader",
+                    options: {
+                        translations: ["en", "de"],
+                    },
+                },
+            },
+            {
+                test: /@openui5[/\\].*\.js$/,
+                use: "openui5-renderer-loader",
+            },
+            {
+                test: /\.xml$/,
+                use: 'openui5-xml-loader',
+            },
         ]
     },
     resolve: {
         extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        modules: [
+            "node_modules/@openui5/sap.m/src",
+            "node_modules/@openui5/sap.ui.core/src",
+            "node_modules/@openui5/sap.ui.core/src/sap/ui/thirdparty", // workaround for signals dependency in hasher
+            "node_modules/@openui5/sap.ui.support/src",
+            "node_modules/@openui5/themelib_sap_belize/src",
+            "node_modules"
+        ],
     },
     output: {
         filename: '[name].js',
@@ -26,6 +53,16 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             title: "GFXBCTS"
-        })
+        }),
+        new OpenUI5Plugin({
+            modulePath: "sap/ui/demo/todo",
+            libs: ["sap.ui.core", "sap.m"],
+            translations: ["en", "de"],
+            theme: ["sap_belize", "sap_belize_plus"],
+            requireSync: [
+                "sap/ui/demo/todo/Component"
+            ],
+            manifest: "./manifest.json"
+        }),
     ]
 };
