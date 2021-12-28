51c680cca906cd9973b0e79a041f687eb49336e4
Stephan Smola <smola@farbspender.de>
2021-12-28 21:30:35 +0100

    Cleanup

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import {Interpreter} from "./runtime/interpreter/Interpreter";
-import {Circle} from "./runtime/gfx/Circle";
-import {Rect} from "./runtime/gfx/Rect";
-
-document.getElementById("ui").innerText = "UPDATE23"
-
-const i = new Interpreter();
-i.addContext("ui", document.getElementById("ui"));
