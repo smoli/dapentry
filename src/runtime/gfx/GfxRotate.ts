982a2eafef057f7ed5db7b9a8cdc73bfb21b88b4
stephan.smola <Stephan.Smola@ilcgroup.com>
2022-01-11 10:22:33 +0100

    FIX: GfxRotate works for ObjectLists now by rotating the last object in the list.

--- src/runtime/gfx/GfxRotate.ts
+++ src/runtime/gfx/GfxRotate.ts
@@ -1,7 +1,8 @@
 import {Operation} from "../interpreter/Operation";
 import {Parameter} from "../interpreter/Parameter";
-import {GrObject} from "../../Geo/GrObject";
+import {GrObject, POI} from "../../Geo/GrObject";
 import {GfxOperation} from "./GfxOperation";
+import {GrObjectList} from "../../Geo/GrObjectList";
 
 export class GfxRotate extends GfxOperation {
     private _angle: Parameter;
@@ -18,6 +19,10 @@ export class GfxRotate extends GfxOperation {
     }
 
     async execute(): Promise<any> {
-        this.target.rotate(this.angle);
+        if (this.target instanceof GrObjectList) {
+            this.target.objects[this.target.objects.length - 1].rotate(this.angle);
+        } else {
+            this.target.rotate(this.angle);
+        }
     }
 }
