f6128b3061a3846abec79120a1dc61448fc16bc8
Stephan Smola <smola@farbspender.de>
2022-01-26 06:36:01 +0100

    Own ColorPickerPopover: ongoing
    We do this, because the standard doesn't provide enough control on how and when the popover is closed.

--- src/controller/Style.controller.ts
+++ src/controller/Style.controller.ts
@@ -3,13 +3,19 @@ import {ComponentController} from "../ComponentController";
 import Component from "../Component";
 import ColorPickerPopover from "sap/ui/unified/ColorPickerPopover";
 import {ColorPickerMode} from "sap/ui/unified/library";
+import Popover from "sap/m/Popover";
+import Toolbar from "sap/m/Toolbar";
+import Button from "sap/m/Button";
+import ColorPicker from "sap/ui/unified/ColorPicker";
+import {ButtonType, PlacementType} from "sap/m/library";
+import ToolbarSpacer from "sap/m/ToolbarSpacer";
 
 /**
  * @namespace sts.drawable.controller
  */
 export default class StyleController extends Controller {
     private fillColorPicker: ColorPickerPopover;
-    private strokeColorPicker: ColorPickerPopover;
+    private strokeColorPicker: Popover;
 
     getComponentController():ComponentController {
         return (this.getOwnerComponent() as Component).getComponentController();
@@ -24,6 +30,8 @@ export default class StyleController extends Controller {
             });
             this.getComponentController().addOperations(code);
         }
+        this.fillColorPicker.destroy();
+        this.fillColorPicker = null;
     }
 
     onStrokeColorChange(event) {
@@ -38,6 +46,8 @@ export default class StyleController extends Controller {
             });
             this.getComponentController().addOperations(code);
         }
+        this.strokeColorPicker.destroy();
+        this.strokeColorPicker = null;
     }
 
     onFillColor(event) {
@@ -45,22 +55,60 @@ export default class StyleController extends Controller {
             this.fillColorPicker = new ColorPickerPopover("stsFillColorPickerPopover", {
                 colorString: "{appModel>style/fillColor}",
                 mode: ColorPickerMode.HSL,
-                change: this.onFillColorChange.bind(this)
+                change: this.onFillColorChange.bind(this),
+
             });
+            this.fillColorPicker.openBy(event.getSource());
+        } else {
+            this.fillColorPicker.close();
+            this.fillColorPicker.destroy();
+            this.fillColorPicker = null;
         }
-        this.fillColorPicker.openBy(event.getSource());
     }
 
     onStrokeColor(event) {
         if (!this.strokeColorPicker) {
+            this.strokeColorPicker = this.makeColorPickerPopOver("{appModel>style/strokeColor}");
+
+/*
             this.strokeColorPicker = new ColorPickerPopover("stsStrokeColorPickerPopover", {
                 colorString: "{appModel>style/strokeColor}",
                 mode: ColorPickerMode.HSL,
                 change: this.onStrokeColorChange.bind(this)
             });
+*/
+            this.strokeColorPicker.openBy(event.getSource(), true);
+        } else {
+            this.strokeColorPicker.close();
+            this.strokeColorPicker.destroy();
+            this.strokeColorPicker = null;
         }
-        this.strokeColorPicker.openBy(event.getSource());
     }
+
+    protected makePopOver(content):Popover {
+        return new Popover({
+            footer: new Toolbar({
+                content: [
+                    new ToolbarSpacer(),
+                    new Button({
+                        text: "OK",
+                        type: ButtonType.Emphasized
+                    })
+                ]
+            }),
+            content: [content],
+            placement: PlacementType.Bottom,
+            showHeader: false
+        });
+    }
+
+    protected makeColorPickerPopOver(forValue: string):Popover {
+        return this.makePopOver(new ColorPicker({
+            colorString: "{appModel>style/strokeColor}",
+            mode: ColorPickerMode.HSL
+        }))
+    }
+
 
     onStrokeWidthChange(event) {
         const selection = this.getComponentController().getSelection();
