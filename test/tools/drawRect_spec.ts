b5bcdf23267692ce01855a4d5112a749bb425632
Stephan Smola <smola@farbspender.de>
2022-01-30 00:38:56 +0100

    Testing Tools.

--- test/tools/drawRect_spec.ts
+++ test/tools/drawRect_spec.ts
@@ -84,13 +84,13 @@ describe('Drawing a rectangle', () => {
 
         drawRect.update(InteractionEvents.Click, makeAClick(100, 100));
         drawRect.update(InteractionEvents.MouseMove, makeAMove(100, 200));
-        drawRect.update(InteractionEvents.Click, makeAClick(200, 200));
+        drawRect.update(InteractionEvents.Click, makeAClick(200, 150));
 
         expect(drawRect.isDone).to.be.true;
         const tokens = Parser.parseLine(drawRect.result);
         expect(tokens[0]).to.deep.equal(T_OPCODE("RECT"))
-        expect(tokens[3]).to.deep.equal(T_POINT_NN(150, 150));
+        expect(tokens[3]).to.deep.equal(T_POINT_NN(150, 125));
         expect(tokens[4]).to.deep.equal(T_NUMBER(100));
-        expect(tokens[5]).to.deep.equal(T_NUMBER(100));
+        expect(tokens[5]).to.deep.equal(T_NUMBER(50));
     });
 });
