ed59d5153645941014b365b39e9f35fa7e642973
Stephan Smola <smola@farbspender.de>
2022-02-13 21:38:44 +0100

    Toolbar: Reflects currently active tool and can be used to switch tools

--- src/ui/ToolBar.vue
+++ src/ui/ToolBar.vue
@@ -1,19 +1,43 @@
 <template>
   <div class="drawable-toolbar">
     <div>
-      <button>C</button>
-      <button>R</button>
-      <button>L</button>
-      <button>P</button>
-      <button>G</button>
-      <button>T</button>
-      <button>S</button>
+      <button v-for="tool in tools"
+              :class="{ 'drawable-tool-active': $store.state.tool.current === tool.name }"
+              @click="onSwitchTool(tool.name)"
+      >
+        {{ tool.buttonContent }}
+      </button>
     </div>
   </div>
 </template>
 
 <script lang="ts">
+import {ToolNames} from "../tools/ToolNames";
+
 export default {
-  name: "ToolBar"
+  name: "ToolBar",
+  inject: ["controller"],
+
+  data() {
+    return {
+      tools: [
+        { buttonContent: "C", name: ToolNames.Circle },
+        { buttonContent: "R", name: ToolNames.Rectangle },
+        { buttonContent: "L", name: ToolNames.Line },
+        { buttonContent: "P", name: ToolNames.Polygon },
+        { buttonContent: "G", name: ToolNames.Move },
+        { buttonContent: "T", name: ToolNames.Rotate },
+        { buttonContent: "S", name: ToolNames.Scale },
+      ]
+    }
+
+  },
+
+  methods: {
+    onSwitchTool(toolName) {
+        this.controller.switchTool(toolName);
+    }
+  }
+
 }
 </script>
