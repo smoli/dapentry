6a1ae04727a9cf9be3cccbc1a497faea79a6f367
Stephan Smola <smola@farbspender.de>
2022-02-16 11:51:42 +0100

    Test: AddStatement action

---
+++ test/actions/addStatement_spec.ts
@@ -0,0 +1,51 @@
+import {describe, it} from "mocha";
+import {expect} from "chai"
+import {AddStatement} from "../../src/actions/AddStatement";
+import {State} from "../../src/state/State";
+import {createAppStore} from "../../src/state/AppStore";
+import {AppController} from "../../src/core/AppController";
+
+
+describe('Add statement action', () => {
+
+    class MockController extends AppController{
+        constructor(state:State) {
+            super(state, null, null)
+        }
+    }
+
+    it('adds a statement at the end', () => {
+        const action = new AddStatement(["ADD r1, 30"]);
+        const state = new State(createAppStore(), null);
+        action.controller = new MockController(state);
+
+        state.setCode(["LOAD r1, 10", "ADD r1, 20"])
+
+        action.execute(null);
+
+        expect(state.store.state.code.code).to.deep.equal([
+            "LOAD r1, 10",
+            "ADD r1, 20",
+            "ADD r1, 30"
+        ])
+    });
+
+
+    it('inserts a statement after the last selected code line', () => {
+        const action = new AddStatement(["ADD r1, 30"]);
+        const state = new State(createAppStore(), null);
+        action.controller = new MockController(state);
+
+        state.setCode(["LOAD r1, 10", "ADD r1, 20"])
+        state.setCodeSelection([0]);
+
+        action.execute(null);
+
+        expect(state.store.state.code.code).to.deep.equal([
+            "LOAD r1, 10",
+            "ADD r1, 30",
+            "ADD r1, 20"
+        ])
+    });
+
+});
