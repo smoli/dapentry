9fa351e28576cbb86052ddbeb82c6f55a31dda71
Stephan Smola <smola@farbspender.de>
2022-02-17 21:56:56 +0100

    Prevent nested loops.
    The runtime supports this. But right now, when iterating over a list of values using FOREACH we're using the list register name as the single value register within the loop. This means we're masking the list register and cannot iterate over it within the same loop. We could use a iteration variable by doing FOREACH v, list. But we don't to make it easier for the user.

--- test/actions/loopStatement_spec.ts
+++ test/actions/loopStatement_spec.ts
@@ -32,7 +32,7 @@ describe('Loop statement', () => {
         ])
     });
 
-    xit("Will wrap code that already contains a loop", () => {
+    it("Will not wrap code that already contains a loop", () => {
         const action = new LoopStatements([1, 2, 3, 4])
         const state = new State(createAppStore(), null);
         action.controller = new MockController(state);
@@ -52,15 +52,13 @@ describe('Loop statement', () => {
         expect(state.store.state.code.code).to.deep.equal([
             "LOAD r1, 10",
             "DO 2",
-            "DO 2",
             "ADD r1, 2",
             "ADD r1, 3",
-            "ENDDO",
             "ENDDO"
         ])
     });
 
-    xit("Will wrap code that contains a forEach", () => {
+    it("Will not wrap code that contains a forEach", () => {
         const action = new LoopStatements([2, 3, 4, 5])
         const state = new State(createAppStore(), null);
         action.controller = new MockController(state);
@@ -81,12 +79,10 @@ describe('Loop statement', () => {
         expect(state.store.state.code.code).to.deep.equal([
             "LOAD r1, 10",
             "LOAD r2, [1, 2, 3, 4]",
-            "DO 2",
             "FOREACH r2",
             "ADD r1, 2",
             "ADD r1, 3",
             "ENDEACH",
-            "ENDDO"
         ])
     });
 
