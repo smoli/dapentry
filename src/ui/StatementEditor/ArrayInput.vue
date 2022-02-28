de26cc4096f7b72d5488a8f86d393a2dbac218fc
Stephan Smola <smola@farbspender.de>
2022-02-28 06:18:35 +0100

    ArrayEditor now only show a single "placeholder value" List(<n>). This allows for replacing this value with a data-field.

--- src/ui/StatementEditor/ArrayInput.vue
+++ src/ui/StatementEditor/ArrayInput.vue
@@ -1,11 +1,25 @@
 <template>
   <span class="drawable-array-input">
 
-  <button class="drawable-array-input-iterator" v-if="itemCount > 1" @click="prevIndex">&lt;</button>
-  <component :is="displayedSegment.type"
-             :content="displayedSegment" @change="onChange" ></component>
-  <button class="drawable-array-input-iterator" v-if="itemCount > 1"  @click="nextIndex">&gt;</button>
-  <span class="drawable-array-input-index">{{displayedIndex}}/{{itemCount}}</span>
+    <span v-if="listMode"
+          @dragenter="onDragEnter"
+          @dragover="onDragOver"
+          @drop="onDrop">
+      <button class="drawable-array-input-iterator" v-if="itemCount > 1" @click="prevIndex">&lt;</button>
+      <component :is="displayedSegment.type"
+                 :content="displayedSegment" @change="onChange"></component>
+      <button class="drawable-array-input-iterator" v-if="itemCount > 1" @click="nextIndex">&gt;</button>
+      <span class="drawable-array-input-index">{{ displayedIndex }}/{{ itemCount }}</span>
+    </span>
+    <span v-else>
+      <GrowingInput :value="placeHolder"
+                    @focus="onSingleRepFocus"
+                    @change="onSingleRepChange"
+                    @dragenter="onDragEnter"
+                    @dragover="onDragOver"
+                    @drop="onDrop"/>
+<!--      <button @click="onSwitchMode">&hellip;</button>-->
+    </span>
   </span>
 </template>
 
@@ -15,27 +29,36 @@ import {createSegmentInfoForToken} from "./createSegments";
 import AtRegisterInput from "./AtRegisterInput.vue";
 import PointInput from "./PointInput.vue";
 import SimpleInput from "./SimpleInput.vue";
+import StringInput from "./StringInput.vue";
 import ExpressionInput from "./ExpressionInput.vue";
 import Static from "./Static.vue";
+import GrowingInput from "./GrowingInput.vue";
+import {deSerializeDNDInfo, DnDDataType, makeDnDHandlers} from "../dnd/DnDInfo";
+
 
 export default {
   name: "ArrayInput",
-  components: { AtRegisterInput, PointInput, NumberInput, SimpleInput, ExpressionInput, Static },
+  components: { GrowingInput, AtRegisterInput, PointInput, NumberInput, StringInput, SimpleInput, ExpressionInput, Static },
   props: ["content"],
   inject: ["controller"],
 
   data() {
     return {
-      displayedIndex: 0
+      displayedIndex: 0,
+      listMode: false
     }
   },
 
   computed: {
+    placeHolder() {
+        return `List(${this.content.token.value.length})`
+    },
+
     displayedSegment() {
-        return createSegmentInfoForToken(
-            this.content.token.value[this.displayedIndex],
-            this.content.statementIndex,
-            [...this.content.subIndexes, this.displayedIndex])
+      return createSegmentInfoForToken(
+          this.content.token.value[this.displayedIndex],
+          this.content.statementIndex,
+          [...this.content.subIndexes, this.displayedIndex])
     },
     itemCount() {
       if (!this.content) {
@@ -46,6 +69,10 @@ export default {
   },
 
   methods: {
+    onSwitchMode() {
+        this.listMode = !this.listMode;
+    },
+
     prevIndex() {
       if (this.displayedIndex > 0) {
         this.displayedIndex -= 1;
@@ -56,7 +83,24 @@ export default {
       if (this.displayedIndex < this.content.token.value.length - 1) {
         this.displayedIndex += 1;
       }
-    }
+    },
+
+    onSingleRepFocus(event) {
+        event.target.value = "";
+    },
+
+    onSingleRepChange(event) {
+      if (event.target.value === "") {
+        return;
+      }
+      this.controller.updateStatement(this.content.statementIndex, this.content.subIndexes, event.target.value);
+    },
+
+    ...makeDnDHandlers(function (event: DragEvent) {
+      event.preventDefault();
+      const info = deSerializeDNDInfo(event.dataTransfer.getData(DnDDataType.ArrayRegister));
+      this.controller.updateStatement(this.content.statementIndex, this.content.subIndexes, info.value1);
+    }, DnDDataType.ArrayRegister),
   }
 }
 </script>
