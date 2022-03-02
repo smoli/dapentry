460cafcaca306f604175da565e8caf804c2149b6
Stephan Smola <smola@farbspender.de>
2022-03-02 23:03:43 +0100

    Make Polygon op always extend existing polygons
    This helps with creating continuous polygons using loops. it of course prevents the creation of polygons for each loop iteration. But I have the feeling that is not necessary.

--- src/runtime/gfx/GfxPolygon.ts
+++ src/runtime/gfx/GfxPolygon.ts
@@ -9,24 +9,32 @@ export class GfxPolygon extends GfxObject {
     private readonly _points: ArrayParameter;
     private readonly _closed: Parameter;
 
-    constructor(opcode:string, target:Parameter, style:Parameter, points:ArrayParameter, closed:Parameter) {
+    constructor(opcode: string, target: Parameter, style: Parameter, points: ArrayParameter, closed: Parameter) {
         super(opcode, target, style);
         this._points = points;
         this._closed = closed;
     }
 
-    get points():Array<Point2D> {
+    get points(): Array<Point2D> {
         return this._points.finalized(this.closure);
     }
 
-    get closed():boolean {
+    get closed(): boolean {
         return !!this._closed.finalized(this.closure);
     }
 
     async execute(interpreter: Interpreter): Promise<any> {
-        const c = GrPolygon.create(this.targetName, this.points, this.closed)
-        c.style = this.style;
-        this.target = c;
+        let poly: GrPolygon = this.target as GrPolygon;
+
+        if (!poly) {
+            poly = GrPolygon.create(this.targetName, this.points, this.closed)
+            poly.style = this.style;
+            this.target = poly;
+        } else {
+            for (const p of this.points) {
+                poly.addPoint(p);
+            }
+        }
     }
 }
 
