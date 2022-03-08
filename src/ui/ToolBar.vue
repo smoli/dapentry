126183b04ca57978d2e795d993617923d7ede461
Stephan Smola <smola@farbspender.de>
2022-03-08 06:29:40 +0100

    Move Icon code to a separate file

--- src/ui/ToolBar.vue
+++ src/ui/ToolBar.vue
@@ -14,6 +14,7 @@
 <script lang="ts">
 import {ToolNames} from "../tools/ToolNames";
 import {ASSERT} from "../core/Assertions";
+import {Icons} from "./css/icons/icons";
 
 export default {
   name: "ToolBar",
@@ -21,55 +22,14 @@ export default {
 
   data() {
     const tools = [
-      { buttonContent: "C", name: ToolNames.Circle, buttonContentSVG: `<svg viewbox="-10 -10 120 120" width="auto" height="auto">
-  <circle cx="50" cy="50" r="50" style="fill:rgb(180,180,180);stroke:black;stroke-width:7" />
-</svg>` },
-      { buttonContent: "R", name: ToolNames.Rectangle, buttonContentSVG: `<svg viewbox="-10 -10 120 120">
-  <rect x="0" y="0" width="100" height="100" style="fill:rgb(180,180,180);stroke:black;stroke-width:7" />
-</svg>` },
-      { buttonContent: "L", name: ToolNames.Line, buttonContentSVG: `<svg viewbox="-10 -10 120 120">
-  <line x1="0" y1="100" x2="100" y2="0" style="fill:rgb(180,180,180);stroke:black;stroke-width:7" />
-</svg>` },
-      { buttonContent: "P", name: ToolNames.Polygon, buttonContentSVG: ` <svg viewbox="-10 -10 120 120">
-  <path d="M0 100 l0 -50 l50 -50 l50 0 l0 100Z" style="fill:rgb(180,180,180);stroke:black;stroke-width:7" />
-</svg>` },
-      { buttonContent: "W", name: ToolNames.Text, buttonContentSVG: `<svg viewbox="-10 -10 120 120">
-  <text x="50" y="95" style="fill:rgb(0,0,0);font-size: 140; font-family: serif; text-anchor:middle">T</text>
-</svg>` },
-      { buttonContent: "G", name: ToolNames.Move, buttonContentSVG: `<svg viewbox="-10 -10 120 120">
- <defs>
-    <marker id="arrowheade" markerWidth="3" markerHeight="4"
-    refX="0" refY="2" orient="auto">
-      <polygon points="0 0, 3 2, 0 4" />
-    </marker>
-   <marker id="arrowheads" markerWidth="3" markerHeight="4"
-    refX="3" refY="2" orient="auto">
-      <polygon points="3 0, 3 4, 0 2" />
-    </marker>
-  </defs>
-   <line x1="10" y1="50" x2="90" y2="50" style="fill:rgb(180,180,180);stroke:black;stroke-width:7" marker-start="url(#arrowheads)" marker-end="url(#arrowheade)" />
-   <line x1="50" y1="10" x2="50" y2="90" style="fill:rgb(180,180,180);stroke:black;stroke-width:7" marker-start="url(#arrowheads)" marker-end="url(#arrowheade)" />
-  </svg>` },
-      { buttonContent: "T", name: ToolNames.Rotate, buttonContentSVG: ` <svg viewbox="-10 -10 120 120">
- <defs>
-    <marker id="arrowheade" markerWidth="3" markerHeight="4"
-    refX="0" refY="2" orient="auto">
-      <polygon points="0 0, 3 2, 0 4" />
-    </marker>
-   </defs>
-   <path d="M100 50 A 45 45 0 1 1 50 10" style="fill: none; stroke:black;stroke-width:7" marker-end="url(#arrowheade)"/>
- </svg>` },
-      { buttonContent: "S", name: ToolNames.Scale, buttonContentSVG: ` <svg viewbox="-10 -10 120 120">
-   <defs>
-    <marker id="arrowheade" markerWidth="3" markerHeight="4"
-    refX="0" refY="2" orient="auto">
-      <polygon points="0 0, 3 2, 0 4" />
-    </marker>
-   </defs>
-  <rect x="0" y="50" width="50" height="50" style="fill:rgb(180,180,180);stroke:black;stroke-width:7" />
-  <rect x="0" y="0" width="100" height="100" style="fill:none;stroke-dasharray:10 10;stroke:black;stroke-width:7" />
-  <line x1="50" y1="50" x2="70" y2="30" style="stroke:black;stroke-width:7" marker-end="url(#arrowheade)"/>
-</svg>` },
+      { buttonContent: "C", name: ToolNames.Circle, buttonContentSVG: Icons.CircleTool },
+      { buttonContent: "R", name: ToolNames.Rectangle, buttonContentSVG: Icons.RectangleTool },
+      { buttonContent: "L", name: ToolNames.Line, buttonContentSVG: Icons.LineTool },
+      { buttonContent: "P", name: ToolNames.Polygon, buttonContentSVG: Icons.PolygonTool },
+      { buttonContent: "W", name: ToolNames.Text, buttonContentSVG: Icons.TextTool },
+      { buttonContent: "G", name: ToolNames.Move, buttonContentSVG: Icons.MoveTool },
+      { buttonContent: "T", name: ToolNames.Rotate, buttonContentSVG: Icons.RotateTool },
+      { buttonContent: "S", name: ToolNames.Scale, buttonContentSVG: Icons.ScaleTool },
     ];
 
     return {
---
+++ src/ui/css/icons/icons.ts
@@ -0,0 +1,59 @@
+export const  Icons = {
+
+    CircleTool: `<svg viewbox="-10 -10 120 120">
+  <circle cx="50" cy="50" r="50" style="fill:rgb(180,180,180);stroke:black;stroke-width:7" />
+</svg>`,
+
+    RectangleTool: `<svg viewbox="-10 -10 120 120">
+  <rect x="0" y="0" width="100" height="100" style="fill:rgb(180,180,180);stroke:black;stroke-width:7" />
+</svg>`,
+
+    LineTool: `<svg viewbox="-10 -10 120 120">
+  <line x1="0" y1="100" x2="100" y2="0" style="fill:rgb(180,180,180);stroke:black;stroke-width:7" />
+</svg>`,
+
+    PolygonTool: ` <svg viewbox="-10 -10 120 120">
+  <path d="M0 100 l0 -50 l50 -50 l50 0 l0 100Z" style="fill:rgb(180,180,180);stroke:black;stroke-width:7" />
+</svg>`,
+
+    TextTool: `<svg viewbox="-10 -10 120 120">
+  <text x="50" y="95" style="fill:rgb(0,0,0);font-size: 140; font-family: serif; text-anchor:middle">T</text>
+</svg>`,
+
+    MoveTool: `<svg viewbox="-10 -10 120 120">
+ <defs>
+    <marker id="arrowheade" markerWidth="3" markerHeight="4"
+    refX="0" refY="2" orient="auto">
+      <polygon points="0 0, 3 2, 0 4" />
+    </marker>
+   <marker id="arrowheads" markerWidth="3" markerHeight="4"
+    refX="3" refY="2" orient="auto">
+      <polygon points="3 0, 3 4, 0 2" />
+    </marker>
+  </defs>
+   <line x1="10" y1="50" x2="90" y2="50" style="fill:rgb(180,180,180);stroke:black;stroke-width:7" marker-start="url(#arrowheads)" marker-end="url(#arrowheade)" />
+   <line x1="50" y1="10" x2="50" y2="90" style="fill:rgb(180,180,180);stroke:black;stroke-width:7" marker-start="url(#arrowheads)" marker-end="url(#arrowheade)" />
+  </svg>`,
+
+    RotateTool: ` <svg viewbox="-10 -10 120 120">
+ <defs>
+    <marker id="arrowheade" markerWidth="3" markerHeight="4"
+    refX="0" refY="2" orient="auto">
+      <polygon points="0 0, 3 2, 0 4" />
+    </marker>
+   </defs>
+   <path d="M100 50 A 45 45 0 1 1 50 10" style="fill: none; stroke:black;stroke-width:7" marker-end="url(#arrowheade)"/>
+ </svg>`,
+
+    ScaleTool: ` <svg viewbox="-10 -10 120 120">
+   <defs>
+    <marker id="arrowheade" markerWidth="3" markerHeight="4"
+    refX="0" refY="2" orient="auto">
+      <polygon points="0 0, 3 2, 0 4" />
+    </marker>
+   </defs>
+  <rect x="0" y="50" width="50" height="50" style="fill:rgb(180,180,180);stroke:black;stroke-width:7" />
+  <rect x="0" y="0" width="100" height="100" style="fill:none;stroke-dasharray:10 10;stroke:black;stroke-width:7" />
+  <line x1="50" y1="50" x2="70" y2="30" style="stroke:black;stroke-width:7" marker-end="url(#arrowheade)"/>
+</svg>`
+}
