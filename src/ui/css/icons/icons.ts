10ce90ce227c8d6d68419d55c76e469556af7242
Stephan Smola <smola@farbspender.de>
2022-03-28 23:06:13 +0200

    Got rid of fontawesome

--- src/ui/css/icons/icons.ts
+++ src/ui/css/icons/icons.ts
@@ -55,5 +55,11 @@ export const Icons = {
   <rect x="0" y="50" width="50" height="50" style="fill:rgb(180,180,180);stroke:black;stroke-width:7" />
   <rect x="0" y="0" width="100" height="100" style="fill:none;stroke-dasharray:10 10;stroke:black;stroke-width:7" />
   <line x1="50" y1="50" x2="70" y2="30" style="stroke:black;stroke-width:7" marker-end="url(#arrowheade)"/>
+</svg>`,
+
+    Pause: `<svg viewBox="-10 -10 120 120">
+  <circle cx="50" cy="50" r="50" style="fill: none; stroke: black; stroke-width:9"/>
+  <line x1="35" y1="25" x2="35" y2="75" style="fill: none; stroke: black; stroke-width:9; stroke-linecap:round"/>
+  <line x1="65" y1="25" x2="65" y2="75" style="fill: none; stroke: black; stroke-width:9; stroke-linecap:round"/>
 </svg>`
 }
